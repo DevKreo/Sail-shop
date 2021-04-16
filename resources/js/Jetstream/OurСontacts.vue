ee5c9df613582a722cbfb93cdd5129bf4a52c7f1
DevKreo <31585723+DevKreo@users.noreply.github.com>
2021-04-16 16:23:52 +0300

    fix padding, add telegram icon

--- resources/js/Jetstream/OurСontacts.vue
+++ resources/js/Jetstream/OurСontacts.vue
@@ -17,15 +17,15 @@
         </button>
     </div>
     <transition name="fade">
-        <div v-if="showOurContacts" class="flex flex-col mb-8">
-            <div class="flex justify-center" v-for="item in content" :key="item.logo">
-                <div class="flex flex-row items-center">
-                    <div class="bg-black text-white rounded-full h-11 w-11 flex items-center justify-center">
+        <div v-if="showOurContacts" class="my-5">
+            <div class="flex justify-center mb-5" v-for="item in content" :key="item.logo">
+                <div class="grid grid-cols-2 gap-1">
+                    <div class="hover:text-black hover:bg-red-550 bg-black text-white rounded-full h-11 w-11 flex items-center justify-center place-self-end self-center">
                         <svg class="w-6 h-6" viewBox="0 0 24 24">
                             <path fill="currentColor" :d="item.logo" />
                         </svg>
                     </div>
-                    <div class="pl-14 flex flex-col justify-items-center">
+                    <div class="pl-14 flex flex-col justify-items-center place-self-center">
                         <span v-for="sentance in item.text">{{sentance}}</span>
                     </div>
                 </div>
@@ -58,7 +58,7 @@ export default {
                     text: ["@horeca_iys"]
                 },
                 {
-                    logo: "M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z",
+                    logo: "M22.05 1.577c-.393-.016-.784.08-1.117.235-.484.186-4.92 1.902-9.41 3.64-2.26.873-4.518 1.746-6.256 2.415-1.737.67-3.045 1.168-3.114 1.192-.46.16-1.082.362-1.61.984-.133.155-.267.354-.335.628s-.038.622.095.895c.265.547.714.773 1.244.976 1.76.564 3.58 1.102 5.087 1.608.556 1.96 1.09 3.927 1.618 5.89.174.394.553.54.944.544l-.002.02s.307.03.606-.042c.3-.07.677-.244 1.02-.565.377-.354 1.4-1.36 1.98-1.928l4.37 3.226.035.02s.484.34 1.192.388c.354.024.82-.044 1.22-.337.403-.294.67-.767.795-1.307.374-1.63 2.853-13.427 3.276-15.38l-.012.046c.296-1.1.187-2.108-.496-2.705-.342-.297-.736-.427-1.13-.444zm-.118 1.874c.027.025.025.025.002.027-.007-.002.08.118-.09.755l-.007.024-.005.022c-.432 1.997-2.936 13.9-3.27 15.356-.046.196-.065.182-.054.17-.1-.015-.285-.094-.3-.1l-7.48-5.525c2.562-2.467 5.182-4.7 7.827-7.08.468-.235.39-.96-.17-.972-.594.14-1.095.567-1.64.84-3.132 1.858-6.332 3.492-9.43 5.406-1.59-.553-3.177-1.012-4.643-1.467 1.272-.51 2.283-.886 3.278-1.27 1.738-.67 3.996-1.54 6.256-2.415 4.522-1.748 9.07-3.51 9.465-3.662l.032-.013.03-.013c.11-.05.173-.055.202-.057 0 0-.01-.033-.002-.026zM10.02 16.016l1.234.912c-.532.52-1.035 1.01-1.398 1.36z",
                     text: ["@bp_infobot"]
                 },
                 {
